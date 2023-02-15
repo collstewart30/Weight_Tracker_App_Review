8a33c9bf38adf88ff894780ac77765152fa4a316
Colleen Stewart <collstewart30@gmail.com>
2023-02-15 16:58:27 -0500

    Installed Bootstrap. Created App.css file. Adding borders components.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
 import AddEntryForm from "./Components/AddEntry/AddEntryForm";
 import EntriesChartTracker from "./Components/EntriesChartTracker/EntriesChartTracker";
+import './App.css';
+
 
 function App() {
   const [entries, setEntries] = useState([
@@ -15,10 +17,22 @@ function App() {
   }
 
   return (
-    <div>
-      <DisplayEntries parentEntries={entries} />
-      <AddEntryForm addNewEntryParent={addNewEntry} />
-      <EntriesChartTracker parentEntries={entries} />
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="border-box">
+            <DisplayEntries parentEntries={entries} />
+          </div>
+          <div className="border-box">
+            <AddEntryForm addNewEntryParent={addNewEntry} />
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className='border-box'>
+            <EntriesChartTracker parentEntries={entries} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
