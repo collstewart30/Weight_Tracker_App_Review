7932738094d117338cd48776c368c9b32cfb969b
Colleen Stewart <collstewart30@gmail.com>
2023-02-15 16:44:12 -0500

    Created EntriesChartTracker component, instantiated in App.js. Added function to pull chart entries from App.js through props.parentEntries to display in chart.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, { useState } from 'react';
-import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
-import AddEntryForm from './Components/DisplayEntries/AddEntry/AddEntryForm';
+import React, { useState } from "react";
+import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
+import AddEntryForm from "./Components/AddEntry/AddEntryForm";
+import EntriesChartTracker from "./Components/EntriesChartTracker/EntriesChartTracker";
 
 function App() {
+  const [entries, setEntries] = useState([
+    { weight: 175, date: "11-23-2021" },
+    { weight: 176, date: "11-24-2021" },
+  ]);
 
-  const[entries, setEntries] = useState([{weight: 175, date: "11-23-2021"},{weight: 176, date: "11-24-2021"}]);
-
-  function addNewEntry(entry){
+  function addNewEntry(entry) {
     let tempEntries = [...entries, entry]; // prevents nesting an array of current entries object, adds as individual objects
-    setEntries(tempEntries);  // coming from form
+    setEntries(tempEntries); // coming from form
   }
 
   return (
     <div>
-      <DisplayEntries parentEntries={entries}/>
-      <AddEntryForm addNewEntryParent={addNewEntry}/>
+      <DisplayEntries parentEntries={entries} />
+      <AddEntryForm addNewEntryParent={addNewEntry} />
+      <EntriesChartTracker parentEntries={entries} />
     </div>
   );
 }
