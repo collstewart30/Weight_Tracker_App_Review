78fdaaa024e4b6b5c19df370d4e11c05c7105ee4
Colleen Stewart <collstewart30@gmail.com>
2023-02-15 15:33:26 -0500

    Created function reference on parent (App.js) to pass addNewEntry function capability to Child (AddEntryForm). Added props.addNewEntryParent function to AddEntryForm's handleSubmit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ function App() {
 
   const[entries, setEntries] = useState([{weight: 175, date: "11-23-2021"},{weight: 176, date: "11-24-2021"}]);
 
+  function addNewEntry(entry){
+    let tempEntries = [...entries, entry]; // prevents nesting an array of current entries object, adds as individual objects
+    setEntries(tempEntries);  // coming from form
+  }
+
   return (
     <div>
       <DisplayEntries parentEntries={entries}/>
-      <AddEntryForm />
+      <AddEntryForm addNewEntryParent={addNewEntry}/>
     </div>
   );
 }
